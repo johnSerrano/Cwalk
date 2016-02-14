bc0858667c7ed49724911079cdaec61d8280bb8d
Julien Barbier <write0@gmail.com>
2016-02-14 04:42:13 +0100

    Updating homepage instructions

--- index.php
+++ index.php
@@ -13,11 +13,12 @@
       <div class="row marketing">
         <div class="col-lg-12">
           <h4>Instructions</h4>
-          <ol>
+          <ol start="0">
             <li>Fill in each box with the expected data.</li>
             <li>Click the "Check" button.</li>
             <li>If correct, a next button will appear to move on.</li>
             <li>If incorrect, review your responses and check again when ready.</li>
+            <li>Have fun!</li>
           </ol>
         </div>
       </div>
