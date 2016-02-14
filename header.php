1ae875166f06aedc527f69733b99e6f38ad29093
augustin francis-boeuf <afrancisboeuf@gmail.com>
2016-02-13 20:17:55 -0800

    3 and 5 dome

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
 	    <?
 	       // Do not show the restart link/button on the homepage
 	       if (basename($_SERVER['PHP_SELF']) != 'index.php') { ?>
-            <li><a href="/">Restart Challenge</a></li>
+            <li><a href="/0_basic_program.php">Restart Challenge</a></li>
 	    <? } ?>
           </ul>
         </nav>
