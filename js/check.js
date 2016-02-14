b3466c108fb6d4699b3fe3482ab4959a5b75f5e2
John Serrano <johnserrano404@gmail.com>
2016-02-13 21:06:29 -0800

    fix things

--- js/check.js
+++ js/check.js
@@ -2,9 +2,17 @@ answers = {
 	"1": ["main"], 
 	"2": [98, "void"], 
 	"3": ["int"],
-	"/basic_program.php": ["int", "main"]
+	"/0_basic_program.php": ["int", "main"],
+	"/1_successful_program.php": [],
+	"/2_return_something.php":[],
+	"/3_discover_integer.php":[],
+	"/4_n_by_number.php":[],
+	"/5_include_library.php":[],
+	"/6_compile_the_file.php":[],
+	"/7_list_all_the_files.php":[],
+	"/8_get_out.php":[]
 }
-
+_
 function correct() {
 	var continuebutton = document.getElementById("continue");
 	continuebutton.style.display = 'block';
@@ -13,7 +21,9 @@ function correct() {
 }
 
 function wrong() {
-	document.getElementById("error").innerHTML="<p>WRONG</p>";
+	var a = document.getElementById("error");
+	a.innerHTML="<p>WRONG</p>";
+	a.style.display = 'block';
 }
 
 function get_inputs() {
@@ -32,9 +42,10 @@ function get_inputs() {
 }
 
 function check() {
-
 	inputs = get_inputs();
 
+	var a = document.getElementById("error");
+	a.style.display = "none";
         if (answers.hasOwnProperty(location.pathname) == false) {
                 console.log("ERROR: invalid question id: " + location.pathname);
                 return;
