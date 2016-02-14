2aba0b92299fff212774e2618f92c0456f54eae5
Julien Barbier <write0@gmail.com>
2016-02-14 07:12:22 +0100

    Adding minimalist end page

---
+++ congrats.php
@@ -0,0 +1,19 @@
+<?
+   $html_title = "return(0);";
+   $html_description = "Congartulations, you just completed all the challenges with success";
+   $page_title = "Congrats!";
+   require_once('header.php');
+?>
+      <div class="jumbotron">
+       <h1>return (0);</h1>
+        <p class="lead">Congratulations!</p>
+        <p><a class="btn btn-lg btn-info" href="0_basic_program.php" role="button">Start Over</a></p>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-12">
+	   <img src="images/orsonclapping.gif">
+        </div>
+      </div>
+
+      <? require_once('footer.php'); ?>
