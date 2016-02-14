48672007285d2c326fe71473839206c0f173fcf6
Julien Barbier <write0@gmail.com>
2016-02-14 04:11:56 +0100

    Adding footer file and index.html -> index.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+      <footer class="footer">
+        <p>
+	  Brought to you by <a target="_blank" href="https://twitter.com/rickharris_dev">Rick Harris</a>,
+	  <a target="_blank" href="https://twitter.com/JohnSerrano2">John Serrano</a>,
+	  <a target="_blank" href="https://twitter.com/afrancisboeuf">Augustin "Gus" Francis-Boeuf</a>,
+	  <a target="_blank" href="https://twitter.com/julienbarbier42">Julien Barbier</a>
+	  | <a target="_blank" href="https://www.holbertonschool.com">Holberton School</a><br>
+	  <a target="_blank" href="https://github.com/johnSerrano/Cwalk">Fork it on GitHub!</a>
+	</p>
+      </footer>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Cwalk | Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="index.html">Restart Challenge</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">C Programming</h3>
+
+      </div>
+
+      <div class="jumbotron">
+        <h1>Basics of C</h1>
+        <p class="lead">Welcome to the Holberton School challenge. This will reinforce your knowledge of C Programming Basics.</p>
+        <p><a class="btn btn-lg btn-success" href="basic_program.html" role="button">Start Challenge</a></p>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <h4>Instructions</h4>
+          <ol>
+            <li>Fill in each box with the expected data.</li>
+            <li>Click the "Check" button.</li>
+            <li>If correct, a next button will appear to move on.</li>
+            <li>If incorrect, review your responses and check again when ready.</li>
+          </ol>
+        </div>
+      </div>
+
+      <? require_once('footer.php'); ?>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
