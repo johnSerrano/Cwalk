fd1767387a788ccef8bdea867713333bfa294fe4
Julien Barbier <write0@gmail.com>
2016-02-14 04:30:58 +0100

    Update author meta

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="<?= $html_description ?>">
-    <meta name="author" content="">
+    <meta name="author" content="Holberton School team">
     <link rel="icon" href="favicon.ico">
 
     <title><?= $html_title ?></title>
