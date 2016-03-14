101e7e4fd0f91aa4dd259ff9dcefc0c320591b58
Julien Barbier <write0@gmail.com>
2016-03-14 12:20:40 -0700

    Style

--- 50_nightmare.php
+++ 50_nightmare.php
@@ -14,14 +14,15 @@ require_once('header.php');
 
 void print(int *a)
 {
-    printf("%lu, %lu, ", sizeof(a), sizeof(*a) );
+    printf("%lu, %lu, ", sizeof(a), sizeof(*a));
 }
 
 int main(void)
 {
     int a[3];
+
     print(a);
-    printf("%lu, %lu\n", sizeof(a), sizeof(*a) );
+    printf("%lu, %lu\n", sizeof(a), sizeof(*a));
     return (0);
 }
 
