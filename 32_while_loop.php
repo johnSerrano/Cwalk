27b2589fd248a01a79193876763327595fec83e6
Rick Houser <21@p6-4.hbtn.io>
2016-03-22 10:14:37 -0700

    fix small typo preventing code from compiling

--- 32_while_loop.php
+++ 32_while_loop.php
@@ -15,7 +15,7 @@ int main(void) {
     int n = 0;
     
     while (n &#60; <input type="text" id="input_0" class="input-small"/>) {
-        printf("%dn");
+        printf("%d", n);
         n++;
     }
     return (0);
